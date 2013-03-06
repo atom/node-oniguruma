dc378d0d74fec983ae2090cc90692d421b4fa367
Kevin Sawicki <kevin@github.com>
2013-03-06 14:10:07 -0800

    Support building oniguruma in 32-bit mode

--- build-onig.sh
+++ build-onig.sh
@@ -1,5 +1,10 @@
 cd vendor/onig
+if [ "$npm_config_arch" == "x86_32" ]; then
+  CPPFLAGS=-m32
+  CFLAGS=-m32
+fi
 ./configure
+make clean
 make
 cp .libs/libonig.a ../../src/libonig.a
 cp oniguruma.h ../../src/oniguruma.h
