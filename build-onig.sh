bf22c8ded6a29e7a5cb01a1fa43e0bcda849e98b
Kevin Sawicki <kevin@github.com>
2013-03-07 14:59:27 -0800

    Set flags on configure line

--- build-onig.sh
+++ build-onig.sh
@@ -1,7 +1,5 @@
 cd vendor/onig
-CPPFLAGS="$1"
-CFLAGS="$1"
-./configure --disable-shared
+CPPFLAGS="$1" CFLAGS="$1" ./configure --disable-shared
 make clean
 make
 cp .libs/libonig.a ../../src/libonig.a
