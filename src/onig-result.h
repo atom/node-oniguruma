ea4f2fdb25b483fe6c468bb3972bd6ab7af67240
Indrek Ardel <indrek@ardel.eu>
2015-03-29 19:39:31 +0300

    Fix indentation

--- src/onig-result.h
+++ src/onig-result.h
@@ -8,21 +8,21 @@ using ::std::string;
 class OnigRegExp;
 
 class OnigResult {
-  public:
-    explicit OnigResult(OnigRegion* region, int indexInScanner);
-    ~OnigResult();
+ public:
+  explicit OnigResult(OnigRegion* region, int indexInScanner);
+  ~OnigResult();
 
-    int Count();
-    int LocationAt(int index);
-    int LengthAt(int index);
-    int Index() { return indexInScanner; }
+  int Count();
+  int LocationAt(int index);
+  int LengthAt(int index);
+  int Index() { return indexInScanner; }
 
-  private:
-    OnigResult(const OnigResult&);  // Disallow copying
-    OnigResult &operator=(const OnigResult&);  // Disallow copying
+ private:
+  OnigResult(const OnigResult&);  // Disallow copying
+  OnigResult &operator=(const OnigResult&);  // Disallow copying
 
-    OnigRegion *region_;
-    int indexInScanner;
+  OnigRegion *region_;
+  int indexInScanner;
 };
 
 #endif  // SRC_ONIG_RESULT_H_
