c337bccd4377a67d6bf993a82fd1da4b52e89a9f
Cheng Zhao <zcbenz@gmail.com>
2015-09-18 23:07:12 +0800

    Fix building with Node 0.10

--- src/onig-string-context.cc
+++ src/onig-string-context.cc
@@ -1,10 +1,10 @@
 #include "onig-string-context.h"
 #include <node.h>  // To see NODE_MINOR_VERSION
 
-OnigStringContext::OnigStringContext(Handle<String> str)
+OnigStringContext::OnigStringContext(Local<String> str)
 // The v8::Persistent API changed recently and now it requires v8::Isolate
 // to the constructor argument.
-  : v8String(Nan::Global<String>(str)),
+  : v8String(str),
     utf8Value(str),
 #ifdef _WIN32
     utf16Value(str),
@@ -12,6 +12,6 @@ OnigStringContext::OnigStringContext(Handle<String> str)
     hasMultibyteCharacters(str->Length() != str->Utf8Length()) {
 }
 
-bool OnigStringContext::IsSame(Handle<String> other) const {
+bool OnigStringContext::IsSame(Local<String> other) {
   return v8String == other;
 }
