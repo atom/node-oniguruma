80716271c26bfc02300d599f83b954676f892a48
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-04-19 14:31:12 -0700

    Convert CoffeeScript to JS

---
+++ benchmark/benchmark.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const {OnigScanner} = require('..')
+
+function runBenchmarkSync (lines, scanner) {
+  let startTime = Date.now()
+  let matches = 0
+
+  for (let row = 0, rowCount = lines.length; row < rowCount; row++) {
+    const line = lines[row]
+    for (let position = 0, length = line.length; position <= length; position++) {
+      if (scanner.findNextMatchSync(line, position)) matches++
+    }
+  }
+
+  console.log(`sync:  ${matches} matches in ${Date.now() - startTime}ms`)
+}
+
+function runBenchmarkAsync (lines, scanner) {
+  let matches = 0
+  let callsInProgress = 0
+
+  function callback (error, match) {
+    if (match != null) { matches++ }
+    if (--callsInProgress === 0) {
+      console.log(`async: ${matches} matches in ${Date.now() - startTime}ms`)
+    }
+  };
+
+  var startTime = Date.now()
+  for (let row = 0, rowCount = lines.length; row < rowCount; row++) {
+    const line = lines[row]
+    for (let position = 0, length = line.length; position <= length; position++) {
+      callsInProgress++
+      scanner.findNextMatch(line, position, callback)
+    }
+  }
+}
+
+console.log('oneline.js')
+runBenchmarkSync(
+  fs.readFileSync(path.join(__dirname, 'oneline.js'), 'utf8').split('\n'),
+  new OnigScanner(['\\[', '\\]', '\\{', '\\}'])
+)
+
+console.log('large.js')
+runBenchmarkSync(
+  fs.readFileSync(path.join(__dirname, 'large.js'), 'utf8').split('\n'),
+  new OnigScanner(['this', 'var', 'selector', 'window'])
+)
+
+runBenchmarkAsync(
+  fs.readFileSync(path.join(__dirname, 'large.js'), 'utf8').split('\n'),
+  new OnigScanner(['this', 'var', 'selector', 'window'])
+)
