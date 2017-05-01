b77195a99f765a0243e47b727021e29a60bab52d
Eduardo Martinez <elingerojo@users.noreply.github.com>
2017-05-01 15:05:31 -0500

    Housekeeping - Removed commented out code

--- src/oniguruma.js
+++ src/oniguruma.js
@@ -3,54 +3,6 @@
 const OnigScanner = require('../build/Release/onig_scanner.node').OnigScanner
 const OnigString = require('../build/Release/onig_scanner.node').OnigString
 
-/*
-class OnigRegExp {
-  constructor (source) {
-    this.source = source.toString()
-    this.scanner = new OnigScanner([this.source])
-  }
-
-  captureIndicesForMatch (string, match) {
-    if (match) {
-      let {captureIndices} = match
-      string = this.scanner.convertToString(string)
-      for (let capture of Array.from(captureIndices)) {
-        capture.match = string.slice(capture.start, capture.end)
-      }
-      return captureIndices
-    } else {
-      return null
-    }
-  }
-
-  searchSync (string, startPosition) {
-    if (startPosition == null) startPosition = 0
-    let match = this.scanner.findNextMatchSync(string, startPosition)
-    return this.captureIndicesForMatch(string, match)
-  }
-
-  search (string, startPosition, callback) {
-    if (startPosition == null) { startPosition = 0 }
-    if (typeof startPosition === 'function') {
-      callback = startPosition
-      startPosition = 0
-    }
-
-    this.scanner.findNextMatch(string, startPosition, (error, match) => {
-      callback(error, this.captureIndicesForMatch(string, match))
-    })
-  }
-
-  testSync (string) {
-    return this.searchSync(string) != null
-  }
-
-  test (string, callback) {
-    this.search(string, 0, (error, result) => callback(error, result != null))
-  }
-}
-*/
-
 function OnigRegExp(source) {
   this.source = source;
   this.scanner = new OnigScanner([this.source]);
